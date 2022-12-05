1ed85117d7b38e624ff36f46201ee56594560b37
Clayton Carlson <claytonrc97@gmail.com>
2022-12-04 21:10:32 -0800

    commit tree c7d1644d637190f0902cd22763b06fb7d8faf715

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(){
+    let newCats = cats.concat("Broom")
+return newCats
+}
+function prependCat(){
+let newCats2 = ["Arnold", ...cats]
+return newCats2
+}
+function removeLastCat(){
+let newCats3 = cats.slice(0, -1)
+return newCats3
+}
+function removeFirstCat(){
+let newCats4 = cats.slice(1, cats.length)
+return newCats4
+}
